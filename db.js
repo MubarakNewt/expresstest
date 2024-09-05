cc46df8e2cc2d543825a0c859bb96b8a8daa8ba1
Mubarak <mubaraknewt10@gmail.com>
2024-09-05 13:17:07 +0100

    update database

---
+++ db.js
@@ -0,0 +1,13 @@
+import pg from 'pg';
+const { Pool } = pg;
+
+const pool = new Pool({
+    user: 'postgres',  // process.env.DB_USER,      
+    host: 'localhost', // process.env.DB_HOST,     
+    database: 'taskman',  // process.env.DB_NAME,  
+    password: 'abcd',  // process.env.DB_PASSWORD,
+    port: 5432 // process.env.DB_PORT
+});
+
+
+export default pool;
--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import express from 'express';
+import taskRoutes from './src/task/routes.js'
 
 
 const app = express();
 const port = 4444
 
+app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send('Hello world')
-})
+
+app.use('/api/v1/taskman', taskRoutes)
+  
 
 app.listen(port, () => console.log(`app on port ${port}`))
